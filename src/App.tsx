59b4999ace16bea72fa3791b47fcb784a7e7995b
sunhwaaRj <pickle02@sookmyung.ac.kr>
2024-11-26 01:40:33 +0900

    Feat: 화면 배치 설정

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,27 @@ import './App.css'
 import styled, { createGlobalStyle } from 'styled-components'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './routes/home'
+import { useEffect } from 'react'
 
 const GlobalStyles = createGlobalStyle`
 body {
     margin: 0;
     background-color: #f2f2f2;
+    display: flex;
+    justify-content: center;
   }
+  :root {
+       --vh: 100%;
+   }
 `
 const Wrapper = styled.div`
   max-width: 390px;
   margin-left: auto;
   margin-right: auto;
   background-color: #FFECEC;
+  display: flex;
+  align-items: center;
+  height: calc(var(--vh, 1vh) * 100);
 `
 
 const router = createBrowserRouter([
@@ -25,6 +34,19 @@ const router = createBrowserRouter([
 
 
 function App() {
+  function setScreenSize() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  }
+  useEffect(() => {
+    setScreenSize();
+    window.addEventListener('resize', setScreenSize);
+    
+    return () => {
+        window.removeEventListener('resize', setScreenSize);
+    };
+}, []);
+
   return (
     <Wrapper className = 'store'>
       <GlobalStyles />
--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,13 +1,31 @@
 import styled from "styled-components"
+import logoimg from "/LOGO.svg"
+import introimg from "/INTRO.svg"
+import KakaoBtn from "../components/kakao"
 
 const Wrapper = styled.div`
-    
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    height: 750px;
+`
+
+const Logo = styled.img`
+    width: 95%;
+`
+
+const Intro = styled.img`
+    margin-top: 40px;
+    margin-bottom: 20px;
 `
 
 export default function Home() {
     return(
         <Wrapper>
-            메인 화면입니다.
+            <Logo src={logoimg} />
+            <Intro src={introimg} />
+            <KakaoBtn />
         </Wrapper>
     )
 }

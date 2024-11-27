2bfb4d2c9c423ef3919b9ee665c3c25e171b4c5a
sunhwaaRj <pickle02@sookmyung.ac.kr>
2024-11-27 14:48:33 +0900

    [#6] Feat: 카카오 인가코드 받아오기

--- src/components/Kakao.tsx
+++ src/components/Kakao.tsx
@@ -21,9 +21,19 @@ const Button = styled.button`
 `
 
 export default function KakaoBtn(){
-    const login = () => {}
+    const login = () => {
+        // 우선 반환값 확인
+        // 카카오 로그인 uri 로 인가코드 포함 리디렉션
+        const apikey = import.meta.env.VITE_KAKAO_REST_API_KEY;
+        const redirectUri = import.meta.env.VITE_KAKAO_REDIRECT_URI
+
+        const kakaoLoginUrl = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${apikey}&redirect_uri=${redirectUri}`;
+
+        // 사용자 리디렉션
+        window.location.href = kakaoLoginUrl;
+    }
     
     return (
-        <Button onClick={() => login} />
+        <Button onClick={login} />
     )
 }

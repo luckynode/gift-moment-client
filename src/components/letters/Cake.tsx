c09d35dafc3d46582f1285cf1952aa83ea4dc2c6
khee2 <21keunghee@gmail.com>
2024-12-01 18:04:59 +0900

    [#2] Rename: 폴더명 소문자 복수형으로 변경

--- src/components/letters/Cake.tsx
+++ src/components/letters/Cake.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import cakeImage from '../../assets/Letter/cake.svg';
+import cakeImage from '../../assets/letters/cake.svg';
 import CakeItem from './CakeItem';
 
 type CakeItemType = {

d12f0270a3a06c61de5f8ad8ee74022bb94aa939
khee2 <21keunghee@gmail.com>
2024-12-03 03:05:01 +0900

    [#2] Feat: 편지 상세보기 모달 생성

---
+++ src/components/letters/LetterDetailModal.tsx
@@ -0,0 +1,89 @@
+import styled from "styled-components";
+
+const ModalBackground = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.4); /* 배경 어둡게 */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000; /* 다른 요소보다 위에 표시 */
+`;
+
+const ModalContent = styled.div`
+  background: linear-gradient(180deg, #FFFFFF 0%, #FFDDEE 80%, #FFFFFF 100%); /* 흰색-핑크-흰색 그라데이션 주기*/
+  border-radius: 30px;
+  width: 270px;
+  height: 460px;
+  padding: 20px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  position: relative;
+`;
+
+const CloseButton = styled.button`
+  position: absolute;
+  top: 15px;
+  right: 15px;
+  background: none;
+  border: none;
+  font-size: 18px;
+  cursor: pointer;
+`;
+
+const ToText = styled.h4`
+  margin: 10px;
+  font-size: 18px;
+  color: black;
+  text-align: left;
+`;
+
+const FromText = styled.h4`
+  margin: 0;
+  font-size: 18px;
+  color: black;
+  text-align: right;
+`;
+
+const ContentBox = styled.div`
+  margin: 20px 0;
+  background-color: #FFF7DF;
+  padding: 20px;
+  border-radius: 20px;
+  white-space: pre-wrap; /* 줄바꿈을 그대로 반영 */
+  font-size: 16px;
+  color: black;
+`;
+
+
+type LetterDetailModalProps = {
+    isOpen: boolean;
+    onClose: () => void;
+    letter: {
+        id: number;
+        to: string;
+        from: string;
+        content: string;
+    } | null;
+};
+
+const LetterDetailModal = ({isOpen, onClose, letter,}: LetterDetailModalProps) => {
+    if (!isOpen || !letter) return null;
+
+    return (
+        <ModalBackground onClick={onClose}>
+            <ModalContent onClick={(e) => e.stopPropagation()}>
+                <CloseButton onClick={onClose}>X</CloseButton>
+                <ToText>TO. {letter.to}</ToText>
+                <ContentBox>
+                    <p>{letter.content}</p>
+                </ContentBox>
+                <FromText>FROM. {letter.from}</FromText>
+            </ModalContent>
+        </ModalBackground>
+    );
+};
+
+export default LetterDetailModal;

3d6130a7279a0db4346de1dcf982d72e53bd6461
sunhwaaRj <pickle02@sookmyung.ac.kr>
2024-12-04 19:13:51 +0900

    [#7] Feat: 사용자 이름 불러오기

--- src/routes/mywish.tsx
+++ src/routes/mywish.tsx
@@ -1,18 +1,44 @@
+import { useEffect, useState } from "react";
 import { Hug18, Submit250, Title, Wrapper } from "../components/SignupComponents";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export default function Mywish() {
+    const [name, setName] = useState("");
+    const navigate = useNavigate();
+
+    useEffect(()=> {
+        const fetchData = async() => {
+            try {
+                // TODO accessToken 가져오기, url 재확인
+
+                const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/mywish`, {
+                    headers: {
+                        // Authorization: `Bearer ${accessToken}`
+                    },
+                });
+                setName(response.data.data);
+            } catch (error) {
+                console.error("Fetch data error : ", error);
+            }
+        };
+        fetchData();
+    }, []);
+
     return(
         <Wrapper>
-            <Title>name 님의 위시리스트</Title>
+            <Title>{name || "name"}님의 위시리스트</Title>
             <Title>확인하러 가볼까요?</Title>
             <Hug18>
                 <Submit250
                     type="button"
                     value="편지함 보러가기"
+                    onClick={() => navigate("/my-letters")}
                 />
                 <Submit250
                     type="button"
                     value="위시리스트 보러가기"
+                    onClick={() => navigate("/signup")}
                 />
             </Hug18>
         </Wrapper>

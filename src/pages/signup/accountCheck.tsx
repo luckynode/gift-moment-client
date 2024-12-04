076c8728bf1ab70d73133fae0b86ece145e8cce5
sunhwaaRj <pickle02@sookmyung.ac.kr>
2024-12-04 16:30:01 +0900

    [#7] Feat: 계좌완료 페이지

--- src/pages/signup/accountCheck.tsx
+++ src/pages/signup/accountCheck.tsx
@@ -1,9 +1,27 @@
+import { styled } from "styled-components";
+import { Submit100, Title, Wrapper } from "../../components/SignupComponents";
+import { useNavigate } from "react-router-dom";
+
+const StyledSubmit100 = styled(Submit100)`
+    margin-top: 40px;
+`;
+
 export default function AccountCheck(){
+    const navigate = useNavigate();
+    
+    const onSubmit = async () => {
+        navigate("/mywish");
+    }
+    
     return(
-        <>
-        <div>
-            계좌 확인 페이지
-        </div>
-        </>
+        <Wrapper>
+            <Title> 계좌가 성공적으로 </Title>
+            <Title> 등록되었어요! </Title>
+            <StyledSubmit100
+                type="button"
+                value="다음"
+                onClick={onSubmit}
+            />
+        </Wrapper>
     )
 }

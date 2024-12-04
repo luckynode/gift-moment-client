49604479983d4f617ec4ec84b322d52a2ab36af6
sunhwaaRj <pickle02@sookmyung.ac.kr>
2024-12-04 17:51:48 +0900

    [#7] Feat: 위시리스트 확인 페이지

--- src/routes/mywish.tsx
+++ src/routes/mywish.tsx
@@ -1,7 +1,20 @@
+import { Hug18, Submit250, Title, Wrapper } from "../components/SignupComponents";
+
 export default function Mywish() {
     return(
-        <>
-        선물 #7 페이지입니다.
-        </>
+        <Wrapper>
+            <Title>name 님의 위시리스트</Title>
+            <Title>확인하러 가볼까요?</Title>
+            <Hug18>
+                <Submit250
+                    type="button"
+                    value="편지함 보러가기"
+                />
+                <Submit250
+                    type="button"
+                    value="위시리스트 보러가기"
+                />
+            </Hug18>
+        </Wrapper>
     )
 }

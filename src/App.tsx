8b795f6c535d5ef0e0d92c2e760eb53e0a17a835
sunhwaaRj <pickle02@sookmyung.ac.kr>
2024-12-10 13:56:06 +0900

    [#16] Design: 배경 높이 수정

--- src/App.tsx
+++ src/App.tsx
@@ -65,7 +65,8 @@ const Wrapper = styled.div`
   background-color: #FFECEC;
   display: flex;
   justify-content: center;
-  height: calc(var(--vh, 1vh) * 100);
+  min-height: calc(var(--vh, 1vh) * 100);
+  height: 100%;
   font-family: 'Pretendard', sans-serif;
 `
 

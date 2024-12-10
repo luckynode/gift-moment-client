0502f29480fa01a277f01703ee26b07acef17183
sunhwaaRj <pickle02@sookmyung.ac.kr>
2024-12-10 15:34:12 +0900

    [#16] Feat: 친구 위시리스트

---
+++ src/components/UserWishItem.tsx
@@ -0,0 +1,55 @@
+import { styled } from "styled-components"
+import { useNavigate } from "react-router-dom"
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 10px;
+    gap: 10px;
+    align-items: center;
+`
+
+const ItemImg = styled.img`
+    width: 250px;
+    height: 180px;
+
+    background: #F8A0BD;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25), inset 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 10px;
+    object-fit: cover;
+`
+
+const Name = styled.div`
+    font-family: 'Lato';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 18px;
+    line-height: 28px;
+    color: #262626;
+`
+
+interface WishItemProps {
+    userid: number;
+    item_id: number;
+    item_image: string;
+    item_name: string;
+}
+
+export default function UserWishItem({userid, item_id, item_image,item_name}: WishItemProps) {
+    const navigate = useNavigate();
+    
+    const handleClick = () => {
+        navigate(`/wishlist/${userid}/item/${item_id}`);
+    };
+
+    return (
+        <>
+            {/* 사진
+                이름 */}
+            <Wrapper onClick={handleClick}>
+                <ItemImg src={item_image}/>
+                <Name>{item_name}</Name>
+           </Wrapper>
+        </>
+    )
+}
--- src/routes/UserWishList.tsx
+++ src/routes/UserWishList.tsx
@@ -2,6 +2,10 @@ import { useParams } from "react-router-dom";
 import { styled } from "styled-components";
 import BackButton from "../components/buttons/BackButton";
 import Header from "../components/headers/Header";
+import eximg from "../assets/wishlist/example.jpg"
+import { useEffect, useState } from "react";
+import axios from "axios";
+import UserWishItem from "../components/UserWishItem";
 
 const Wrapper = styled.div`
     display: flex;
@@ -20,18 +24,80 @@ const Subtitle = styled.div`
     background-clip: text;
 `
 
+const ListWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 40px 0;
+    gap: 40px;
+`
+
+// TODO 백 api 명세서 확인 후 재구성
+interface WishListData {
+    userid: number;
+    name: string;
+    birth: string;
+    dday: number;
+    items: Array<{
+        item_id: number;
+        item_image: string;
+        item_name: string;
+    }>;
+}
+
 export default function UserWishList() {
     const { userId } = useParams<{ userId: string }>();
+    const [wishData, setWishData] = useState<WishListData>({
+        userid: 1,
+        name: "김친구",
+        birth: "00월 00일",
+        dday: 0,
+        items: [
+            { item_id: 1, item_image: eximg, item_name: "아이폰1" },
+            { item_id: 2, item_image: eximg, item_name: "아이폰2"},
+            { item_id: 3, item_image: eximg, item_name: "아이폰3"},
+            { item_id: 4, item_image: eximg, item_name: "아이폰4"},
+            { item_id: 5, item_image: eximg, item_name: "아이폰5"},
+        ],
+    });
+
+
+    // TODO FetchData 주석 제거
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/wishlist/${userId}`,  {
+                    headers: {
+                        // Authorization: `Bearer ${accessToken}`, // Authorization 헤더에 토큰 포함
+                    },
+                });
+                setWishData(response.data.data);
+            } catch (error) {
+                console.error("Fetching Data Error: ", error);
+            }
+        };
+
+        // fetchData();
+    }, []);
 
     return (
         <>
             <BackButton />
             <Wrapper>
-                <Subtitle>00월 00일 D-00</Subtitle>
-                <Header title="000님의 위시리스트" />
-                {/* map 으로 개수만큼 반복 */}
-                {/* 사진
-                이름 */}
+                <Subtitle>{wishData?.birth || '00월 00일'} D-{wishData?.dday || '00'}</Subtitle>
+                <Header title={`${wishData?.name || '김친구'}님의 위시리스트`} /> 
+                <ListWrapper>
+                    {/* map 으로 개수만큼 반복 */}
+                    {wishData?.items.map((items) => (
+                        <UserWishItem
+                            key={items.item_id}
+                            userid={wishData.userid}
+                            item_id={items.item_id}
+                            item_image={items.item_image}
+                            item_name={items.item_name}
+                        />
+                    ))}
+                </ListWrapper>
             </Wrapper>
         </>
     )

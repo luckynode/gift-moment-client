1e74282b07954acb967b350149b33c65673992ca
sunhwaaRj <pickle02@sookmyung.ac.kr>
2024-12-11 23:51:10 +0900

    [#16] Feat: 축하해준 친구들 리스트

--- src/pages/UserWishCheck.tsx
+++ src/pages/UserWishCheck.tsx
@@ -75,6 +75,70 @@ const CheckBtn = styled.div`
     gap: 20px;
 `
 
+const Congrats = styled.div`
+    background-color: white;
+    min-width: 390px;
+    padding: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+const CongratsTitle = styled.div`
+    font-size: 20px;
+    font-weight: 700;
+    margin-bottom: 20px;
+`
+
+const List = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: flex-start;
+    width: 363px;
+    gap: 15px;
+`
+
+const Card = styled.div`
+    width: 111px;
+    height: 50px;
+    display: flex;
+    flex-direction: row;
+    gap: 5px;
+    margin-bottom: 5px;
+`
+
+const CardImg = styled.img`
+    border-radius: 50%;
+    background-color: blue;
+    width: 50px;
+    height: 50px;
+`
+
+const CardName = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: flex-start;
+`
+
+const FriendName = styled.div`
+    font-family: 'Lato';
+    font-weight: 700;
+    font-size: 12px;
+    text-align: center;
+    color: #000000;
+`
+
+const FreindPercent = styled.div`
+    font-family: 'Lato';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 12px;
+    text-align: center;
+    color: #000000;
+`
+
 // TODO 백 api 명세서 확인 후 재구성
 interface UserWishData {
     userid: number;
@@ -87,11 +151,16 @@ interface UserWishData {
     item_price: number;
     item_link: string;
     item_info: string;
+    Friends : Array<{
+        friend_profile: string;
+        friend_name: string;
+        friend_percent: number;
+    }>
 }
 
 export default function UserWishCheck() {
     const navigate = useNavigate();
-    const { userId } = useParams<{ userId: string }>();
+    const { userId, itemId } = useParams<{ userId: string, itemId: string }>();
     const [wishData, setWishData] = useState<UserWishData>({
         userid: 1,
         name: "김친구",
@@ -102,14 +171,21 @@ export default function UserWishCheck() {
         item_name: "아이폰1",
         item_price: 1000000,
         item_link: "https://www.apple.com/kr/",
-        item_info: "선물 소개란입니다."
+        item_info: "선물 소개란입니다.",
+        Friends: [
+            { friend_profile: "", friend_name: "김친구", friend_percent: 50 },
+            { friend_profile: "", friend_name: "김친구", friend_percent: 50 },
+            { friend_profile: "", friend_name: "김친구", friend_percent: 50 },
+            { friend_profile: "", friend_name: "김친구", friend_percent: 50 },
+            { friend_profile: "", friend_name: "김친구", friend_percent: 50 },
+        ]
     });
 
     // TODO FetchData 주석 제거 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/wishlist/${userId}`,  {
+                const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/wishlist/${userId}/item/${itemId}`,  {
                     headers: {
                         // Authorization: `Bearer ${accessToken}`, // Authorization 헤더에 토큰 포함
                     },
@@ -145,6 +221,21 @@ export default function UserWishCheck() {
                     <Button text="좋아요!" size="small" color="black" onClick={() => {navigate("send")}}/>
                     <Button text="괜찮아요" size="small" color="white" onClick={() => {}}/>
                 </CheckBtn>
+                <Congrats>
+                    <CongratsTitle>축하해준 친구들</CongratsTitle>
+                    <List>
+                        {/* map으로 개수만큼 반복 */}
+                        {wishData?.Friends.map((friend) => (
+                            <Card>
+                                <CardImg src={friend.friend_profile}/>
+                                <CardName>
+                                    <FriendName>{friend.friend_name}</FriendName>
+                                    <FreindPercent>{friend.friend_percent}%</FreindPercent>
+                                </CardName>
+                            </Card>
+                        ))}
+                    </List>
+                </Congrats>
             </Wrapper>
         </>
     )

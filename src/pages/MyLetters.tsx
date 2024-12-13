ebe277e609588899872e769f757fd11ca1250543
khee2 <21keunghee@gmail.com>
2024-12-13 22:28:31 +0900

    [#21] Refactor: 장신구 이미지 배열을 별도의 상수 파일로 분리

--- src/pages/MyLetters.tsx
+++ src/pages/MyLetters.tsx
@@ -6,61 +6,12 @@ import Cake from '../components/letters/Cake';
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import LetterDetailModal from "../components/letters/LetterDetailModal";
+import { ornamentImages } from '../assets/ornamentImages';
+import { ornamentPositions } from '../assets/ornamentImages';
 
-import redFlower from '../assets/letters/red_flower.svg';
-import whiteStar from '../assets/letters/white_star.svg';
-import orangeFlower from '../assets/letters/orange_flower.svg';
-import sparkle from '../assets/letters/sparkle.svg';
-import rainbowStar from '../assets/letters/rainbow_star.svg';
-import cherry from '../assets/letters/cherry.svg';
-import cloud from '../assets/letters/cloud.svg';
-import blueEnvelope from '../assets/letters/blue_envelope.svg';
-import mintEnvelope from '../assets/letters/mint_envelope.svg';
-import whiteFlower from '../assets/letters/white_flower.svg';
-import strawberry from '../assets/letters/strawberry.svg';
-import heart from '../assets/letters/heart.svg';
-import yellowMusic from '../assets/letters/yellow_music.svg';
-import blackMusic from '../assets/letters/black_music.svg';
 import InstructionText from "../components/InstructionText.tsx";
 import {useNavigate} from "react-router-dom";
 import BackButton from "../components/buttons/BackButton.tsx";
-
-// 장신구 이미지 경로 배열
-const ornamentImages = [
-    redFlower,
-    whiteStar,
-    orangeFlower,
-    sparkle,
-    rainbowStar,
-    cherry,
-    cloud,
-    blueEnvelope,
-    mintEnvelope,
-    whiteFlower,
-    strawberry,
-    heart,
-    yellowMusic,
-    blackMusic,
-];
-
-// 장신구 위치 배열
-// FIXME 장신구 위치는 px로 잡으면 반응형 어려워짐. %로 변경하는 것 고려해봐야 할듯
-const positions = [
-    {top: '125px', left: '55px'},  // redFlower
-    {top: '122px', left: '120px'},  // whiteStar
-    {top: '115px', left: '187px'},  // orangeFlower
-    {top: '120px', left: '245px'},  // sparkle
-    {top: '190px', left: '52px'},  // rainbowStar
-    {top: '185px', left: '122px'},  // cherry
-    {top: '185px', left: '185px'},  // cloud
-    {top: '180px', left: '252px'},  // blueEnvelope
-    {top: '250px', left: '40px'},  // mintEnvelope
-    {top: '245px', left: '115px'},  // whiteFlower
-    {top: '245px', left: '185px'},  // strawberry
-    {top: '245px', left: '250px'},  // heart
-    {top: '45px', left: '36px'},  // yellowMusic
-    {top: '45px', left: '260px'},  // blackMusic
-];
 
 const ButtonContainer = styled.div`
   display: flex;
@@ -135,8 +86,8 @@ const MyLetters = () => {
     const items = ornamentImages.slice(0, letterCount).map((src, index) => ({
         id: index,
         src,
-        top: positions[index].top,
-        left: positions[index].left,
+        top: ornamentPositions[index].top,
+        left: ornamentPositions[index].left,
         onClick: () => handleItemClick(index),
     }));
 

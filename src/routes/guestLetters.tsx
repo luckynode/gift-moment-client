6e54ce9c522c8c8cb7bf62b254d3a43503668ea7
khee2 <21keunghee@gmail.com>
2025-01-27 14:20:15 +0900

    [#32] Feat: 게스트 편지 조회 api 값 할당하기
    - 생일 이전에만 편지 작성 및 위시리스트 조회가 가능하도록 함

--- src/routes/guestLetters.tsx
+++ src/routes/guestLetters.tsx
@@ -4,16 +4,14 @@ import Header from '../components/headers/Header.tsx';
 import Button from '../components/buttons/Button.tsx';
 import Cake from '../components/letters/Cake.tsx';
 import "react-toastify/dist/ReactToastify.css";
-import { ornamentImages } from '../assets/ornamentImages.ts';
-import { ornamentPositions } from '../assets/ornamentImages.ts';
+import {ornamentImages} from '../assets/ornamentImages.ts';
+import {ornamentPositions} from '../assets/ornamentImages.ts';
 
 import InstructionText from "../components/InstructionText.tsx";
 import {useNavigate, useParams} from "react-router-dom";
 import BackButton from "../components/buttons/BackButton.tsx";
-import axios from "axios";
 import {getGuestLetters} from "../apis/guestLetterApi.ts";
 import {toast} from "react-toastify";
-import success = toast.success;
 
 export const Wrapper = styled.div`
   display: flex;
@@ -32,9 +30,8 @@ const ButtonContainer = styled.div`
 `;
 
 
-// NOTE 현재는 하드코딩된 값 사용. 나중에 동적으로 처리 필요
 const GuestLetters = () => {
-    const { uniqueString } = useParams();
+    const {uniqueString} = useParams();
     const navigate = useNavigate();
 
     const [ownerName, setOwnerName] = useState('');
@@ -59,7 +56,7 @@ const GuestLetters = () => {
                     if (response.status === "success") {
                         console.log("게스트 편지 조회 성공:", response.data);
 
-                        const { birthday_owner_name, before_birthday, total_letters} = response.data;
+                        const {birthday_owner_name, before_birthday, total_letters} = response.data;
                         setOwnerName(birthday_owner_name);
                         setBeforeBirthday(before_birthday);
                         setLetterCount(total_letters);
@@ -76,18 +73,27 @@ const GuestLetters = () => {
 
     return (
         <>
-        <Wrapper>
-            {/*TODO 백엔드 API 유저 정보 받아오기*/}
-            <BackButton/>
-            <Header title="경희님의 편지함"/>
-            <Cake items={items}/> {/* items를 Cake에 전달 */}
-            <InstructionText iconText="Notice" message={`생일을 함께 축하해주세요!\n편지 작성 후 선물 전달이 가능합니다!`}/>
-            <ButtonContainer>
-                <Button type="button" text="편지 쓰러 가기" size="large" color="white"
-                        onClick={() => navigate("/write-letter")}/>
-                <Button type="button" text="위시리스트 보러 가기" size="large" color="black" onClick={() => navigate("/")}/>
-            </ButtonContainer>
-        </Wrapper>
+            <Wrapper>
+                <BackButton/>
+                <Header title={`${ownerName}님의 편지함`}/>
+                <Cake items={items}/> {/* items를 Cake에 전달 */}
+                <InstructionText
+                    iconText="Notice"
+                    message={
+                        beforeBirthday
+                            ? `생일을 함께 축하해주세요!\n편지 작성 후 선물 전달이 가능합니다!`
+                            : `편지 전달 기간이 종료되었습니다!`
+                    }
+                />
+                {beforeBirthday ? (
+                    <ButtonContainer>
+                        <Button type="button" text="편지 쓰러 가기" size="large" color="white"
+                                onClick={() => navigate("/write-letter", {state: {uniqueString}})}/>
+                        <Button type="button" text="위시리스트 보러 가기" size="large" color="black"
+                                onClick={() => navigate("/", {state: {uniqueString}})}/>
+                    </ButtonContainer>
+                ) : ""}
+            </Wrapper>
         </>
     );
 };

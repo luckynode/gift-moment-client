1d8d4bf91b80877f0353c8e9879e55766c9a8388
khee2 <21keunghee@gmail.com>
2025-01-28 23:03:32 +0900

    [#44] Deploy: 백엔드 주소에 /api 붙은 VITE_BACKEND_URL_PREFIX 생성

--- src/apis/guestLetterApi.ts
+++ src/apis/guestLetterApi.ts
@@ -2,14 +2,14 @@ import axios, {AxiosResponse} from 'axios';
 import {ApiResponse} from '../types/common/apiResponse';
 import {AddLetterRequest, GetGuestLettersResponse} from "../types/api/guestLetter.ts";
 
-const BASE_URL = import.meta.env.VITE_BACKEND_URL;
+const BASE_URL_PREFIX = import.meta.env.VITE_BACKEND_URL_PREFIX;
 
 export const addLetter = async (
     uniqueString: string,
     data: AddLetterRequest
 ): Promise<ApiResponse<{}>> => {
     const response: AxiosResponse<ApiResponse<{}>> = await axios.post(
-        `${BASE_URL}/api/v1/letters/create/${uniqueString}`,
+        `${BASE_URL_PREFIX}/v1/letters/create/${uniqueString}`,
         data,
         {
             headers: {
@@ -25,7 +25,7 @@ export const getGuestLetters = async (
     uniqueString: string
 ): Promise<ApiResponse<GetGuestLettersResponse>> => {
     const response: AxiosResponse<ApiResponse<GetGuestLettersResponse>> = await axios.get(
-        `${BASE_URL}/api/v1/letters/guestview/${uniqueString}`
+        `${BASE_URL_PREFIX}/v1/letters/guestview/${uniqueString}`
     );
     return response.data;
 };

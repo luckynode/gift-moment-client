69f990314666b32d6424fe4263f79ed4b256b5a7
khee2 <21keunghee@gmail.com>
2025-01-30 21:47:13 +0900

    [#49] Fix: JWT 토큰을 API 호출 시점에서 가져오도록 수정
    - API 호출 시 최신 JWT 토큰을 가져오도록 변경
    - 401 Unauthorized 오류 해결

--- src/apis/myLetterApi.ts
+++ src/apis/myLetterApi.ts
@@ -3,9 +3,8 @@ import {CopyLetterUrlResponse, GetMyLettersResponse} from "../types/api/myLetter
 import axios, {AxiosResponse} from 'axios';
 
 const BASE_URL = import.meta.env.VITE_BACKEND_URL;
-const jwt_token = localStorage.getItem('jwt_token');
-
 export const copyLetterUrl = async (): Promise<ApiResponse<CopyLetterUrlResponse>> => {
+    const jwt_token = localStorage.getItem('jwt_token');
     const response: AxiosResponse<ApiResponse<CopyLetterUrlResponse>> = await axios.get(
         `${BASE_URL}/api/v1/letters/copy`,
         {
@@ -18,6 +17,7 @@ export const copyLetterUrl = async (): Promise<ApiResponse<CopyLetterUrlResponse
 };
 
 export const getMyLetters = async (): Promise<ApiResponse<GetMyLettersResponse>> => {
+    const jwt_token = localStorage.getItem('jwt_token');
     const response: AxiosResponse<ApiResponse<GetMyLettersResponse>> = await axios.get(
         `${BASE_URL}/api/v1/letters`,
         {
